52bf6a7352dd69d60db1f4fd0f3bc85240813063
trio-programming <63399043+trio-programming@users.noreply.github.com>
2020-05-18 11:53:26 +0200

    many errors corrections

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main () {
   printf("Calcul des années bissextiles \n ------- \n code par trio-programming\n\n");
   int annee = 0, reste = 0;
   printf("Quelle annee voluez-vous verifier ?\n");
-  scanf("%d", annee);
+  scanf("%d", &annee);
   
   reste = annee % 4;
   if(reste != 0) {printf("Cette année n'est pas bissextile.\n");}
@@ -13,7 +13,7 @@ int main () {
     reste = annee % 100;
     if(reste == 0) {
       reste = annee % 400;
-      if (reste == 0) {printf("Cette annee est bien bissextile\n";}
+      if (reste == 0) {printf("Cette annee est bien bissextile\n");}
       else {printf("Cette année n'est pas bissextile.\n");}
     }
     printf("Cette année est bien bissextile.\n");
